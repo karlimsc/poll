74119cd9d6496612e9ece5d445e99b82a136d462
karlimsc <karlianamsuarez@gmail.com>
2020-11-11 18:58:14 -0400

    update casi listo

--- src/components/DatatableComp.js
+++ src/components/DatatableComp.js
@@ -9,7 +9,6 @@ import Check from '@material-ui/icons/Check';
 import ChevronLeft from '@material-ui/icons/ChevronLeft';
 import ChevronRight from '@material-ui/icons/ChevronRight';
 import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
 import Edit from '@material-ui/icons/Edit';
 import FilterList from '@material-ui/icons/FilterList';
 import FirstPage from '@material-ui/icons/FirstPage';
@@ -18,6 +17,7 @@ import Remove from '@material-ui/icons/Remove';
 import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 import axios from 'axios'
 import Alert from '@material-ui/lab/Alert';
 
@@ -25,7 +25,7 @@ const tableIcons = {
   Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
   Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
   Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <FileCopyIcon {...props} ref={ref} />),
   DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
   Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
   Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
@@ -45,6 +45,8 @@ const api = axios.create({
   baseURL: `http://localhost:8081`
 })
 
+const client= { "idClient" : sessionStorage.getItem("id")}
+
 function validateEmail(email){
   const re = /^\S+@\S+\.\S+$/;
   return re.test(String(email).toLowerCase());
@@ -55,8 +57,10 @@ function DataTableComp() {
   var columns = [
     {title: "id", field: "id_auth", hidden: true},
     {title: "Avatar", render: rowData => <Avatar maxInitials={1} size={40} round={true} name={rowData === undefined ? " " : rowData.name} />  },
-    {title: "First name", field: "name"},
-    {title: "email", field: "email"}
+    {title: "Name", field: "name"},
+    {title: "Email", field: "email"},
+    {title: "Code", field: "password"},
+    {title: "Status", field: "status"}
   ]
   const [data, setData] = useState([]); //table data
 
@@ -68,7 +72,6 @@ function DataTableComp() {
     api.get("/authorities")
         .then(res => {
             setData(res.data)
-            console.log(res.data);
          })
          .catch(error=>{
              console.log("Error")
@@ -77,52 +80,22 @@ function DataTableComp() {
 
   const handleRowUpdate = (newData, oldData, resolve) => {
     //validation
-    let errorList = []
+  
+    let errorList = [];
+
     if(newData.name === ""){
       errorList.push("Please enter first name")
     }
     if(newData.email === "" || validateEmail(newData.email) === false){
       errorList.push("Please enter a valid email")
     }
+    if(newData.status === ""){
+      errorList.push("Please enter status")
+    }
 
     if(errorList.length < 1){
-      api.patch("/authorities/"+newData.id_auth, newData)
-      .then(res => {
-        const dataUpdate = [...data];
-        const index = oldData.tableData.id_auth;
-        dataUpdate[index] = newData;
-        setData([...dataUpdate]);
-        resolve()
-        setIserror(false)
-        setErrorMessages([])
-      })
-      .catch(error => {
-        setErrorMessages(["Update failed! Server error"])
-        setIserror(true)
-        resolve()
 
-      })
-    }else{
-      setErrorMessages(errorList)
-      setIserror(true)
-      resolve()
-
-    }
-
-  }
-
-  const handleRowAdd = (newData, resolve) => {
-    //validation
-    let errorList = []
-    if(newData.name === undefined){
-      errorList.push("Please enter first name")
-    }
-    if(newData.email === undefined || validateEmail(newData.email) === false){
-      errorList.push("Please enter a valid email")
-    }
-
-    if(errorList.length < 1){ //no error
-      api.post("/users", newData)
+      api.put("/authority/update/"+oldData.id_auth, newData)
       .then(res => {
         let dataToAdd = [...data];
         dataToAdd.push(newData);
@@ -139,8 +112,30 @@ function DataTableComp() {
     }else{
       setErrorMessages(errorList)
       setIserror(true)
-      resolve()
+      resolve()}
+
+  }
+
+  const handleRowAdd = (newData, resolve) => {
+    //validation
+    let errorList = []
+    if(newData.name === undefined){
+      errorList.push("Please enter first name")
     }
+    if(newData.email === undefined || validateEmail(newData.email) === false){
+      errorList.push("Please enter a valid email")
+    }
+    if(newData.password === undefined){
+      errorList.push("Please enter code")
+    }
+    if(newData.status === undefined){
+      errorList.push("Please enter status")
+    }
+
+    if(errorList.length < 1){ //no error
+
+        console.log(newData);
+      }
 
 
   }
@@ -164,7 +159,7 @@ function DataTableComp() {
 
 
   return (
-    <div className="App">
+    <div className="App margin-top" >
 
       <Grid container spacing={1}>
           <Grid item xs={3}></Grid>
@@ -179,7 +174,7 @@ function DataTableComp() {
             }
           </div>
             <MaterialTable
-              title="User data from remote source"
+              title="Autoridades"
               columns={columns}
               data={data}
               icons={tableIcons}
